cefd79a7823cf4625a072ee32d5842c21e8527d5
ivan <ibodnar@outlook.com>
2018-11-21 19:14:20 -0300

    Importado projectsArray a ProjectsListComponent

--- src/app/components/projects-list/projects-list.component.ts
+++ src/app/components/projects-list/projects-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import ProjectModel from '../../models/project.model';
+import projectsArray from '../../data/projects.db';
+
+
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.css']
 })
 export class ProjectsListComponent implements OnInit {
+  projects: ProjectModel[] = projectsArray;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
