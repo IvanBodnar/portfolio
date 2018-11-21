6843721415cbf86b4716548ee89519c1d3abaf61
ivan <ibodnar@outlook.com>
2018-11-21 16:21:29 -0300

    Se importa skillsArray en SkillsListComponent y se usa en ngFor con skills-detail - La <div class="progress-item"> se pasa a skills-detail

--- src/app/components/skills-list/skills-list.component.ts
+++ src/app/components/skills-list/skills-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import SkillModel from '../../models/skill.model';
+import skillsArray from '../../data/skills.db';
+
+
 @Component({
   selector: 'app-skills-list',
   templateUrl: './skills-list.component.html',
   styleUrls: ['./skills-list.component.css']
 })
 export class SkillsListComponent implements OnInit {
+  skills: SkillModel[] = skillsArray;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {  }
 
 }
