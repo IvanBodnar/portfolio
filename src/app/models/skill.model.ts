965cb850c136f8d0b9a4d4cd61c2f22759ddb415
ivan <ibodnar@outlook.com>
2018-11-21 16:13:12 -0300

    Se dejan sólo los campos title y percentage en SkillModel

--- src/app/models/skill.model.ts
+++ src/app/models/skill.model.ts
@@ -2,9 +2,6 @@
 export default class SkillModel {
   constructor(
     public title: string,
-    public minValue: number,
-    public maxValue: number,
-    public valueNow: number,
-    public percentage: number
+    public percentage: string
   ) {}
 }
