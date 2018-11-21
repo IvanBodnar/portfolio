8a70399fb0a3c1b7a406a0038463e6606e7ac208
ivan <ibodnar@outlook.com>
2018-11-21 16:19:25 -0300

    Se agrega @Input() SkillModel en skills-detail y se usa en html

--- src/app/components/skills-detail/skills-detail.component.ts
+++ src/app/components/skills-detail/skills-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import SkillModel from '../../models/skill.model';
 
 @Component({
   selector: 'app-skills-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills-detail.component.css']
 })
 export class SkillsDetailComponent implements OnInit {
+  @Input() skill: SkillModel;
 
   constructor() { }
 
