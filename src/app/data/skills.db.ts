e641b838006b96edc66b9fefd791374bd6d4a0a5
ivan <ibodnar@outlook.com>
2018-11-21 16:14:53 -0300

    Se agrega directorio data -> skills.db.ts -> array de SkillModel para usar en skill-detail

---
+++ src/app/data/skills.db.ts
@@ -0,0 +1,20 @@
+import SkillModel from '../models/skill.model';
+
+export default [
+  {
+    title: 'JavaScript (ES6)',
+    percentage: '60'
+  },
+  {
+    title: 'Angular',
+    percentage: '70'
+  },
+  {
+    title: 'Node',
+    percentage: '70'
+  },
+  {
+    title: 'Python',
+    percentage: '70'
+  },
+] as  SkillModel[];
