4dcc4cc13e9221653230956149c7121a6adf0886
ivan <ibodnar@outlook.com>
2018-11-21 19:05:47 -0300

    Agregado array de ProjectModel -> projects.db.ts

---
+++ src/app/data/projects.db.ts
@@ -0,0 +1,10 @@
+import ProjectModel from '../models/project.model';
+
+export default [
+  {
+    id: 'geocoder',
+    title: 'Geocodificador CABA',
+    text: 'App para ubicar intersecciones de CABA, hecho con Angular, Node y Postgis.',
+    imgUrl: 'assets/img/geocoder1.png'
+  }
+] as ProjectModel[];
