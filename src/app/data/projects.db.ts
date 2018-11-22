c4a354a55366975641dacbc01b3b9af53c6e086c
ivan <ibodnar@outlook.com>
2018-11-22 16:39:03 -0300

    Agregados valores de prueba en projects.db para los nuevos campos explanationText y technologiesUsed de ProjectModel

--- src/app/data/projects.db.ts
+++ src/app/data/projects.db.ts
@@ -5,6 +5,15 @@ export default [
     id: 'geocoder',
     title: 'Geocodificador CABA',
     text: 'App para ubicar intersecciones de CABA, hecho con Angular, Node y Postgis.',
-    imgUrl: 'assets/img/geocoder1.png'
+    imgUrl: 'assets/img/geocoder1.png',
+    explanationText: `
+      Geocodificador para CABA realizado con Postgis.
+    `,
+    technologiesUsed: [
+      'angular',
+      'node',
+      'postgresql',
+      'postgis'
+    ]
   }
 ] as ProjectModel[];
