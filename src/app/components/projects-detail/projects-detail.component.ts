0f74c25d03e10f968b0efb7d1a25144feec27d63
ivan <ibodnar@outlook.com>
2018-11-22 22:18:33 -0300

    Se saca media query para 900px en projects-detail y se manejan alturas

--- src/app/components/projects-detail/projects-detail.component.ts
+++ src/app/components/projects-detail/projects-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 import ProjectModel from '../../models/project.model';
