2246a3f8c35695bdab4440a598eba0d89ea3cf1a
ivan <ibodnar@outlook.com>
2018-11-23 13:47:58 -0300

    Navbar link proyectos hace scrollIntoView a la parte de las cards de proyectos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit() {
+  }
+
+  onProjects() {
+    const projects: HTMLDivElement = this.renderer.selectRootElement('.card-project', true);
+    projects.scrollIntoView({ behavior: 'smooth' });
   }
 
 }
