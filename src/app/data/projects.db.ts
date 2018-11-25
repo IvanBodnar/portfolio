92d6cf664196f99198afe99301d1141fed303937
ivan <ibodnar@outlook.com>
2018-11-25 13:57:06 -0300

    Agregado proyecto en projects.db.ts - Resize de imágenes para que sean iguales (286x160) - Se comitean las png

--- src/app/data/projects.db.ts
+++ src/app/data/projects.db.ts
@@ -5,7 +5,7 @@ export default [
     id: 'geocoder',
     title: 'Geocodificador CABA',
     text: 'App para ubicar intersecciones de CABA, hecho con Angular, Node y Postgis.',
-    imgUrl: 'assets/img/geocoder1.png',
+    imgUrl: 'assets/img/geocoder.png',
     projectUrl: 'https://www.geocodificador-caba.online/',
     explanationText: `
       Geocodificador para CABA realizado con Postgis.
@@ -15,6 +15,21 @@ export default [
       'node',
       'postgresql',
       'postgis'
+    ]
+  },
+  {
+    id: 'cards',
+    title: 'Memory Cards',
+    text: 'Aplicación para estudio, utiliza tarjetas para memorizar conceptos.',
+    imgUrl: 'assets/img/cards.png',
+    projectUrl: 'https://memorycards.netlify.com/',
+    explanationText: `
+      En esta app se pueden crear temas, y en cada tema se crean tarjetas.
+    `,
+    technologiesUsed: [
+      'angular',
+      'node',
+      'mongodb'
     ]
   }
 ] as ProjectModel[];
