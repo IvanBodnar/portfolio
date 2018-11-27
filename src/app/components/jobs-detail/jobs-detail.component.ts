4326d77082e39c7d6e7aab7658dc5c17f009d5e8
ivan <ibodnar@outlook.com>
2018-11-26 23:28:39 -0300

    JobsDetailComponent sirve el array de JobModel que trae de jobs-service

--- src/app/components/jobs-detail/jobs-detail.component.ts
+++ src/app/components/jobs-detail/jobs-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {JobsService} from '../../services/jobs.service';
+import JobModel from '../../models/job.model';
+
 
 @Component({
   selector: 'app-jobs-detail',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobs-detail.component.css']
 })
 export class JobsDetailComponent implements OnInit {
+  jobId: string;
+  job: JobModel;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private jobsService: JobsService
+  ) { }
 
   ngOnInit() {
+    this.jobId = this.route.snapshot.params.jobsId;
+    this.job = this.jobsService.jobsArray.find(
+      ( job: JobModel ) => job.id === this.jobId
+    );
+    window.scrollTo(0, 0);
   }
 
 }
