f31e1e0a628f05e6765f0d33dec2ea197baf9344
ivan <ibodnar@outlook.com>
2018-11-27 13:04:20 -0300

    Se cambia condición de color directive para no cubrir pantallas con poco scroll

--- src/app/directives/color-scroll.directive.ts
+++ src/app/directives/color-scroll.directive.ts
@@ -1,5 +1,6 @@
 import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
+
 interface ColorConfig {
   defaultColor: string;
   newColor: string;
@@ -23,8 +24,8 @@ export class ColorScrollDirective {
     const scroll = window.scrollY;
     const height = document.body.clientHeight;
     // breakPoint -> a partir de donde cambia al color nuevo
-    const breakPoint = height < 650 ? 3000 : height > 650 && height < 1000 ? 1800 : 2100;
-    if (scroll > 400 && scroll < breakPoint) {
+    const breakPoint = height < 850 ? 3000 : height > 850 && height < 1000 ? 1800 : 2100;
+    if (scroll > height && scroll > 400 && scroll < breakPoint) {
       this._setColor(this.config.newColor);
     } else if (scroll > breakPoint) {
       this._setColor(this.config.defaultColor);
@@ -34,6 +35,7 @@ export class ColorScrollDirective {
     // Test
     // console.log(scroll, height);
   }
+
 
   private _setColor(color: string): void {
     this.renderer.setStyle(this.el.nativeElement, 'background', color);
