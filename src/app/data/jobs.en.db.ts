44f3255cfbff4414e878df4c56f395bb07f07dab
ivan <ibodnar@outlook.com>
2019-01-02 18:19:49 -0300

    Se agrega jobs.db versión inglés

---
+++ src/app/data/jobs.en.db.ts
@@ -0,0 +1,34 @@
+import JobModel from '../models/job.model';
+
+
+export default [
+  {
+    id: 'observatorio',
+    title: 'Transit Safety Observatory - CABA',
+    description: `
+      On the Transit Safety Observatory me desempeñé como administrador de
+      bases de datos (Postgres), desarrollador y analista de datos geográficos. Desarrollé varias
+      aplicaciones, tanto de escritorio como web, usando PyQt y Django.
+    `,
+    competences: [
+      'Mantenimiento de bases de datos',
+      'Procesamiento de datos',
+      'Tareas de geoprocesamiento con Postgis, Arcgis y Qgis'
+    ]
+  },
+  {
+    id: 'anachronics',
+    title: 'Anachronics SRL',
+    description: `
+      En Anachronics SRL me desempeñé como desarrollador backend, programando en plpgsql
+      y SQL.\n
+      Mis tareas incluían asimismo mantener reuniones telefónicas con clientes, para acordar
+      el delineamiento de proyectosm para luego implementarlos en código. 
+    `,
+    competences: [
+      'Desarrollo backend en general',
+      'Delineamiento de proyectos con clientes',
+      'Desarrollo de features requeridas por el cliente'
+    ]
+  }
+] as JobModel[];
