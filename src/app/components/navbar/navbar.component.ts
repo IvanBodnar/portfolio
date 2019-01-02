3449cd5886f306f18c5340dfaaaa8d648077842e
ivan <ibodnar@outlook.com>
2019-01-02 17:19:42 -0300

    Navbar -> Se agrega select en navbar y se asocia con onSelectLang method

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import {Component, HostListener, OnInit, Renderer2} from '@angular/core';
 import {Router} from '@angular/router';
 
+import {TranslationService} from '../../services/translation.service';
+import Languages from '../../models/language.enum';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +13,7 @@ export class NavbarComponent implements OnInit {
   navbarMobile: HTMLDivElement;
 
   constructor(
+    private translationService: TranslationService,
     private renderer: Renderer2,
     public router: Router
   ) { }
@@ -21,6 +25,13 @@ export class NavbarComponent implements OnInit {
   onProjects() {
     const projects: HTMLDivElement = this.renderer.selectRootElement('.card-project', true);
     projects.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  onSelectLang( event: Event ): void {
+    // @ts-ignore
+    const selectValue = event.target.value;
+    const selectedLanguage = selectValue === 'Español' ? Languages.es : selectValue === 'English' ? Languages.en : null;
+    this.translationService.changeLanguage( selectedLanguage );
   }
 
   @HostListener('document:click', ['$event'])
