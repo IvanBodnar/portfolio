67efbd9c83a5474021dc8d85536b987d4381f445
ivan <ibodnar@outlook.com>
2019-01-02 17:18:35 -0300

    TranslationService -> se implementa BehaviorSubject para lenguaje y changeLanguage method

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+import Languages from '../models/language.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
+  private _languageSubject = new BehaviorSubject<Languages>(Languages.es);
+  language$ = this._languageSubject.asObservable();
 
   constructor() { }
+
+  changeLanguage( lang: Languages ): void {
+    this._languageSubject.next( lang );
+  }
 }
