bea1322205d5406500b3d2ea78487b194e653223
ivan <ibodnar@outlook.com>
2019-01-02 21:32:55 -0300

    Traducción projects.db english

--- src/app/data/projects.en.db.ts
+++ src/app/data/projects.en.db.ts
@@ -3,16 +3,16 @@ import ProjectModel from '../models/project.model';
 export default [
   {
     id: 'geocoder',
-    title: 'Geocodificador CABA',
-    text: 'App para ubicar intersecciones de CABA, hecho con Angular, Node y Postgis.',
+    title: 'CABA geocoder',
+    text: 'App that finds intersections of CABA, made with Angular, Node and Postgis.',
     imgUrl: 'assets/img/geocoder.png',
     projectUrl: 'https://www.geocodificador-caba.online/',
     explanationText: `
-      Geocoder para CABA realizado a través de funciones geográficas provistas
-      por Postgis. Actualmente pueden buscarse en la aplicación solo intersecciones,
-      pero próximamente voy a implementar también la búsqueda por altura.\n
-      El deployment del backend y la base de datos está hecho mediante docker, en tanto que el frontend
-      se encuentra alojado en Netlify.
+      Autonomous City of Buenos Aires Geocoder, made with geographic functions provided
+      by Postgis. Currently searches can only be made by intersection,
+      but soon search by street number will also be implemented.\n
+      The Database and backend deployment are made through Docker, while the frontend
+      is hosted on Netlify.
     `,
     technologiesUsed: [
       'angular',
@@ -25,18 +25,18 @@ export default [
   {
     id: 'cards',
     title: 'Memory Cards',
-    text: 'Aplicación para estudio, utiliza tarjetas para memorizar conceptos sobre distintos temas.',
+    text: 'Study app, uses cards to memorize concepts about different subjects.',
     imgUrl: 'assets/img/cards.png',
     projectUrl: 'https://memorycards.netlify.com/',
     explanationText: `
-      En esta app se pueden crear tarjetas divididas por temas, para luego pasarlas una a una e ir
-      memorizándolas. Las tarjetas tienen un frente, donde se puede especificar una pregunta, y un dorso 
-      donde escribir la respuesta. Asimismo se cuenta con la posibilidad de indicarle al sistema si
-      el usuario acertó o no, dato que es utilizado por el backend para reordenar las tarjetas de acuerdo
-      a la cantidad veces que fueron contestadas correctamente. De este modo el sistema muestra mas veces 
-      las preguntas menos acertadas.\n
-      El deployment del backend y la base de datos está hecho mediante docker, en tanto que el frontend
-      se encuentra alojado en Netlify.
+      On this app theme grouped cards can be created, so then can be memorized by sifting
+      through them. Cards have a front, where a question can be specified, and a backside 
+      where the answer can be written. There's also the chance to indicate the system if
+      the user answered correctly or not, data that is used by the backend to reorder the cards according
+      the number of times that each question was correctly answered. This way, the system shows more frequently 
+      the less correctly answered quesions.\n
+      The Database and backend deployment are made through Docker, while the frontend
+      is hosted on Netlify.
     `,
     technologiesUsed: [
       'angular',
